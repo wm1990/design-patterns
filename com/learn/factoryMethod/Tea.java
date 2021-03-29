e00fe0be46f5a46d3623a53b86be5d0f4d58adc8
wm1990 <39044206+wm1990@users.noreply.github.com>
2021-03-29 22:48:31 +0800

    Create Tea.java

---
+++ com/learn/factoryMethod/Tea.java
@@ -0,0 +1,14 @@
+package com.learn.factoryMethod;
+
+/**
+ * @ProjectName: [design-patterns]
+ * @Package: com.learn.factoryMethod
+ * @ClassName: Tea
+ * @Description:产品接口
+ * @Author: [wangmeng]
+ * @CreateDate: 2021/3/29 11:03
+ * @Version: V1.0
+ */
+public interface Tea {
+    void make();
+}
