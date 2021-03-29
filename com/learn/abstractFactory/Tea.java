80f6ab7e8d81ad67bb87df9ceb91c84059f34782
wm1990 <39044206+wm1990@users.noreply.github.com>
2021-03-29 22:50:22 +0800

    Create Tea.java

---
+++ com/learn/abstractFactory/Tea.java
@@ -0,0 +1,14 @@
+package com.learn.abstractFactory;
+
+/**
+ * @ProjectName: [lh-tms1.0]
+ * @Package: com.timesoft.constant
+ * @ClassName: Tea 产品接口
+ * @Description:
+ * @Author: [wangmeng]
+ * @CreateDate: 2021/3/27 21:45
+ * @Version: V1.0
+ */
+public interface Tea {
+    void make();
+}
