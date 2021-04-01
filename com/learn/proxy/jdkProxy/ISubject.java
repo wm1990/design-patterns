4b7f62e391a638e6d023ba61bf661562f35407d4
wm1990 <39044206+wm1990@users.noreply.github.com>
2021-04-01 16:25:29 +0800

    Create ISubject.java

---
+++ com/learn/proxy/jdkProxy/ISubject.java
@@ -0,0 +1,14 @@
+package com.learn.proxy.jdkProxy;
+
+/**
+ * @ProjectName: [design-patterns]
+ * @Package: com.learn.proxy.jdkProxy
+ * @ClassName: ISubject
+ * @Description:抽象主体类
+ * @Author: [wangmeng]
+ * @CreateDate: 2021/4/1 16:08
+ * @Version: V1.0
+ */
+public interface ISubject {
+    void request();
+}
