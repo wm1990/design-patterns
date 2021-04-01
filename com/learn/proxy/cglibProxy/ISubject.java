0e1d3d5f30984c8e766be833a6b174efc19f0251
wm1990 <39044206+wm1990@users.noreply.github.com>
2021-04-01 16:26:06 +0800

    Create ISubject.java

---
+++ com/learn/proxy/cglibProxy/ISubject.java
@@ -0,0 +1,14 @@
+package com.learn.proxy.cglibProxy;
+
+/**
+ * @ProjectName: [design-patterns]
+ * @Package: com.learn.proxy.cglibProxy
+ * @ClassName: ISubject
+ * @Description:抽象主体类
+ * @Author: [wangmeng]
+ * @CreateDate: 2021/4/1 16:11
+ * @Version: V1.0
+ */
+public interface ISubject {
+    void request();
+}
