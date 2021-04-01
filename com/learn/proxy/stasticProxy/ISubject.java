3f958c071a53e2adee87898ddb32821aaf9eb127
wm1990 <39044206+wm1990@users.noreply.github.com>
2021-04-01 16:24:41 +0800

    Create ISubject.java

---
+++ com/learn/proxy/stasticProxy/ISubject.java
@@ -0,0 +1,14 @@
+package com.learn.proxy.stasticProxy;
+
+/**
+ * @ProjectName: [design-patterns]
+ * @Package: com.learn.proxy.stasticProxy
+ * @ClassName: ISubject
+ * @Description:抽象主体类
+ * @Author: [wangmeng]
+ * @CreateDate: 2021/4/1 16:05
+ * @Version: V1.0
+ */
+public interface ISubject {
+    void request();
+}
