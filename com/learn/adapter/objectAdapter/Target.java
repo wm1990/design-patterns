8ee5c8b9de0c5acbf0ac5c03319c60aeba8e3dbf
wm1990 <39044206+wm1990@users.noreply.github.com>
2021-04-07 12:29:14 +0800

    Create Target.java

---
+++ com/learn/adapter/objectAdapter/Target.java
@@ -0,0 +1,14 @@
+package com.learn.adapter.objectAdapter;
+
+/**
+ * @ProjectName: [design-patterns]
+ * @Package: com.learn.adapter.objectAdapter
+ * @ClassName: Target
+ * @Description:目标角色
+ * @Author: [wangmeng]
+ * @CreateDate: 2021/4/7 10:52
+ * @Version: V1.0
+ */
+public interface Target {
+    void request();
+}
