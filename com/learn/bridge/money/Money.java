4cb97dde56cb424822f13284285d4e44437bd13c
wm1990 <39044206+wm1990@users.noreply.github.com>
2021-04-07 12:38:18 +0800

    Create Money.java

---
+++ com/learn/bridge/money/Money.java
@@ -0,0 +1,18 @@
+package com.learn.bridge.money;
+
+/**
+ * @ProjectName: [design-patterns]
+ * @Package: com.learn.bridge.money
+ * @ClassName: Money
+ * @Description:实现角色
+ * @Author: [wangmeng]
+ * @CreateDate: 2021/4/7 11:35
+ * @Version: V1.0
+ * Copyright: 北京宏图天安 Copyright (c) 2021
+ */
+public interface Money {
+    //奖金类型
+    String getMoneyType();
+    //金额
+    Double getMoneyAmount();
+}
