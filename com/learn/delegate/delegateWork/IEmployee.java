aaa96056032487b82708e23de69198931bdb525c
wm1990 <39044206+wm1990@users.noreply.github.com>
2021-04-07 13:14:39 +0800

    Create IEmployee.java

---
+++ com/learn/delegate/delegateWork/IEmployee.java
@@ -0,0 +1,14 @@
+package com.learn.delegate.delegateWork;
+
+/**
+ * @ProjectName: [design-patterns]
+ * @Package: com.learn.delegate.delegateWork
+ * @ClassName: IEmployee
+ * @Description:员工接口
+ * @Author: [wangmeng]
+ * @CreateDate: 2021/3/31 23:05
+ * @Version: V1.0
+ */
+public interface IEmployee {
+    void work(String task);
+}
