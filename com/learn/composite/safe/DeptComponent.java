bf8df79a5d6055e7f38045551db573bce4b5e586
wm1990 <39044206+wm1990@users.noreply.github.com>
2021-04-07 12:57:58 +0800

    Create DeptComponent.java

---
+++ com/learn/composite/safe/DeptComponent.java
@@ -0,0 +1,15 @@
+package com.learn.composite.safe;
+
+/**
+ * @ProjectName: [design-patterns]
+ * @Package: com.learn.Composite.transparent
+ * @ClassName: Component
+ * @Description:
+ * @Author: [wangmeng]
+ * @CreateDate: 2021/3/30 22:51
+ * @Version: V1.0
+ * Copyright: 北京宏图天安 Copyright (c) 2021
+ */
+public interface DeptComponent {
+    void getName();
+}
