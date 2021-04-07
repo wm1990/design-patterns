2285bc5607aa54affdbf69f2175a0a9ac53211e1
wm1990 <39044206+wm1990@users.noreply.github.com>
2021-04-07 12:30:36 +0800

    Create Target.java

---
+++ com/learn/adapter/interfaceAdapter/Target.java
@@ -0,0 +1,18 @@
+package com.learn.adapter.interfaceAdapter;
+
+/**
+ * @ProjectName: [design-patterns]
+ * @Package: com.learn.adapter.interfaceAdapter
+ * @ClassName: Target
+ * @Description:目标角色
+ * @Author: [wangmeng]
+ * @CreateDate: 2021/4/7 10:55
+ * @Version: V1.0
+ */
+public interface Target {
+    void request1();
+    void request2();
+    void request3();
+    void request4();
+    void request5();
+}
