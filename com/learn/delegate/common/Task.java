bfdfd7b427e19ce7ee65ff02248db704bdc38886
wm1990 <39044206+wm1990@users.noreply.github.com>
2021-04-07 13:13:12 +0800

    Create Task.java

---
+++ com/learn/delegate/common/Task.java
@@ -0,0 +1,14 @@
+package com.learn.delegate.common;
+
+/**
+ * @ProjectName: [design-patterns]
+ * @Package: com.learn.delegate.common
+ * @ClassName: Task
+ * @Description:抽象任务角色
+ * @Author: [wangmeng]
+ * @CreateDate: 2021/3/31 22:47
+ * @Version: V1.0
+ */
+public interface Task {
+    void doTask();
+}
