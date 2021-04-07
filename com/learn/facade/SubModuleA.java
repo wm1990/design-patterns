7bf1e40abaceaa3db9a09928cb1f7f87bac49809
wm1990 <39044206+wm1990@users.noreply.github.com>
2021-04-07 10:22:24 +0800

    Create SubModuleA.java

---
+++ com/learn/facade/SubModuleA.java
@@ -0,0 +1,16 @@
+package com.learn.facade;
+
+/**
+ * @ProjectName: [design-patterns]
+ * @Package: com.learn.facade
+ * @ClassName: SubModuleA
+ * @Description:子模块A
+ * @Author: [wangmeng]
+ * @CreateDate: 2021/4/7 10:18
+ * @Version: V1.0
+ */
+public class SubModuleA {
+    public void doA(){
+        System.out.println("子模块A的被调用！");
+    }
+}
