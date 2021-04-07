25838deb7e3e5a0ad6d0a14f85ce5ef39b9e5971
wm1990 <39044206+wm1990@users.noreply.github.com>
2021-04-07 13:08:22 +0800

    Update DeptComponent.java

--- com/learn/composite/safe/DeptComponent.java
+++ com/learn/composite/safe/DeptComponent.java
@@ -8,7 +8,6 @@ package com.learn.composite.safe;
  * @Author: [wangmeng]
  * @CreateDate: 2021/3/30 22:51
  * @Version: V1.0
- * Copyright: 北京宏图天安 Copyright (c) 2021
  */
 public interface DeptComponent {
     void getName();
