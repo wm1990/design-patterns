5bf7d6e58edec17ead71cf8bba1a8b8f7b07ea30
wm1990 <39044206+wm1990@users.noreply.github.com>
2021-04-07 13:27:28 +0800

    Create ITransport.java

---
+++ com/learn/strategy/transport/ITransport.java
@@ -0,0 +1,14 @@
+package com.learn.strategy.transport;
+
+/**
+ * @ProjectName: [design-patterns]
+ * @Package: com.learn.strategy.transport
+ * @ClassName: ITransport
+ * @Description:交通方式接口
+ * @Author: [wangmeng]
+ * @CreateDate: 2021/4/1 23:57
+ * @Version: V1.0
+ */
+public interface ITransport {
+    void goOut();
+}
