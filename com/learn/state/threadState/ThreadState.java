b8b8a04db79fd2c50cd3dcd2f03f54bed9064a82
wm1990 <39044206+wm1990@users.noreply.github.com>
2021-04-07 13:44:54 +0800

    Create ThreadState.java

---
+++ com/learn/state/threadState/ThreadState.java
@@ -0,0 +1,15 @@
+package com.learn.state.threadState;
+
+/**
+ * @ProjectName: [design-patterns]
+ * @Package: com.learn.state.threadState
+ * @ClassName: ThreadState
+ * @Description:线程状态抽象类
+ * @Author: [wangmeng]
+ * @CreateDate: 2021/4/6 16:44
+ * @Version: V1.0
+ */
+public abstract class ThreadState {
+    //状态名称
+    protected String stateName;
+}
