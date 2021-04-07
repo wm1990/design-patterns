d3c2fe7a9743f8cce20a2ecc1bfa07fe5fc50b52
wm1990 <39044206+wm1990@users.noreply.github.com>
2021-04-07 10:39:18 +0800

    Create Component.java

---
+++ com/learn/decorator/common/Component.java
@@ -0,0 +1,14 @@
+package com.learn.decorator.common;
+
+/**
+ * @ProjectName: [design-patterns]
+ * @Package: com.learn.decorator
+ * @ClassName: Component
+ * @Description:抽象构件
+ * @Author: [wangmeng]
+ * @CreateDate: 2021/4/7 10:26
+ * @Version: V1.0
+ */
+public interface Component {
+    void doingSomeThing();
+}
