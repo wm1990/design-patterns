a222361bb91ec838ed85e63d74dc20eba53338e5
wm1990 <39044206+wm1990@users.noreply.github.com>
2021-04-07 13:26:03 +0800

    Create Strategy.java

---
+++ com/learn/strategy/common/Strategy.java
@@ -0,0 +1,14 @@
+package com.learn.strategy.common;
+
+/**
+ * @ProjectName: [design-patterns]
+ * @Package: com.learn.strategy.common
+ * @ClassName: Strategy
+ * @Description:抽象策略类
+ * @Author: [wangmeng]
+ * @CreateDate: 2021/4/1 23:36
+ * @Version: V1.0
+ */
+public interface Strategy {
+    void strategyMethod();
+}
