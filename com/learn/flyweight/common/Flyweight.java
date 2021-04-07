42a21e1162253a49c06a614572e4450716d7c20c
wm1990 <39044206+wm1990@users.noreply.github.com>
2021-04-07 12:52:38 +0800

    Create Flyweight.java

---
+++ com/learn/flyweight/common/Flyweight.java
@@ -0,0 +1,14 @@
+package com.learn.flyweight.common;
+
+/**
+ * @ProjectName: [design-patterns]
+ * @Package: com.learn.flyweight
+ * @ClassName: Flyweight
+ * @Description:抽象享元角色
+ * @Author: [wangmeng]
+ * @CreateDate: 2021/3/29 23:03
+ * @Version: V1.0
+ */
+public interface Flyweight {
+    void operation(String outState);
+}
