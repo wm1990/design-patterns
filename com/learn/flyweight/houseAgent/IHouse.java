a17f2924a1f6475696c494706e6e5a358d134e68
wm1990 <39044206+wm1990@users.noreply.github.com>
2021-04-07 12:54:39 +0800

    Create IHouse.java

---
+++ com/learn/flyweight/houseAgent/IHouse.java
@@ -0,0 +1,14 @@
+package com.learn.flyweight.houseAgent;
+
+/**
+ * @ProjectName: [design-patterns]
+ * @Package: com.learn.flyweight.houseAgent
+ * @ClassName: IHouse
+ * @Description:房源接口
+ * @Author: [wangmeng]
+ * @CreateDate: 2021/3/29 23:35
+ * @Version: V1.0
+ */
+public interface IHouse {
+    void showMsg(String anency);
+}
