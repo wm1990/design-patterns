2afd05608c5cdf6c898665780ce6e60796b3662b
wm1990 <39044206+wm1990@users.noreply.github.com>
2021-04-07 13:49:18 +0800

    Create Observer.java

---
+++ com/learn/observer/common/Observer.java
@@ -0,0 +1,14 @@
+package com.learn.observer.common;
+
+/**
+ * @ProjectName: [design-patterns]
+ * @Package: com.learn.observer.common
+ * @ClassName: Observer
+ * @Description:抽象观察者
+ * @Author: [wangmeng]
+ * @CreateDate: 2021/4/6 21:54
+ * @Version: V1.0
+ */
+public interface Observer {
+    void update();
+}
