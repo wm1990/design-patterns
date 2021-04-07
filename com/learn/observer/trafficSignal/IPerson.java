fbf871b1ec0de1d0912737933c2d914bb74e971e
wm1990 <39044206+wm1990@users.noreply.github.com>
2021-04-07 13:51:21 +0800

    Create IPerson.java

---
+++ com/learn/observer/trafficSignal/IPerson.java
@@ -0,0 +1,17 @@
+package com.learn.observer.trafficSignal;
+
+import java.awt.*;
+
+/**
+ * @ProjectName: [design-patterns]
+ * @Package: com.learn.observer.trafficSignal
+ * @ClassName: Person
+ * @Description:抽象观察者
+ * @Author: [wangmeng]
+ * @CreateDate: 2021/4/6 22:12
+ * @Version: V1.0
+ */
+public interface IPerson{
+    //通过
+    void across(Color color);
+}
