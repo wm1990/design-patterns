fb1acab05668e41d78a6504fa8b29f2f88fa7121
wm1990 <39044206+wm1990@users.noreply.github.com>
2021-04-07 12:35:36 +0800

    Create Implementor.java

---
+++ com/learn/bridge/common/Implementor.java
@@ -0,0 +1,14 @@
+package com.learn.bridge.common;
+
+/**
+ * @ProjectName: [design-patterns]
+ * @Package: com.learn.bridge.common
+ * @ClassName: Implementor
+ * @Description:实现角色
+ * @Author: [wangmeng]
+ * @CreateDate: 2021/4/7 11:27
+ * @Version: V1.0
+ */
+public interface Implementor {
+    void doSomeThing();
+}
