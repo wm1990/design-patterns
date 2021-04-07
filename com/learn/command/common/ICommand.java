61c75a5fefe24b3458a4d137b70c0fe02024904a
wm1990 <39044206+wm1990@users.noreply.github.com>
2021-04-07 13:36:57 +0800

    Create ICommand.java

---
+++ com/learn/command/common/ICommand.java
@@ -0,0 +1,14 @@
+package com.learn.command.common;
+
+/**
+ * @ProjectName: [design-patterns]
+ * @Package: com.learn.command.common
+ * @ClassName: ICommand
+ * @Description:抽象命令
+ * @Author: [wangmeng]
+ * @CreateDate: 2021/4/4 14:51
+ * @Version: V1.0
+ */
+public interface ICommand {
+    void command();
+}
