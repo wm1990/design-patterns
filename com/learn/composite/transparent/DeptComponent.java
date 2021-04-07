6141f45165428ec16b359f2bd89e3e20c6c5cb99
wm1990 <39044206+wm1990@users.noreply.github.com>
2021-04-07 13:09:49 +0800

    Update DeptComponent.java

--- com/learn/composite/transparent/DeptComponent.java
+++ com/learn/composite/transparent/DeptComponent.java
@@ -10,7 +10,6 @@ import java.util.List;
  * @Author: [wangmeng]
  * @CreateDate: 2021/3/30 22:51
  * @Version: V1.0
- * Copyright: 北京宏图天安 Copyright (c) 2021
  */
 public interface DeptComponent {
     void add(DeptComponent deptComponent);
