5f59857c46ef83e71fba7ab17eafa1d64424462d
wm1990 <39044206+wm1990@users.noreply.github.com>
2021-04-07 13:38:51 +0800

    Create ICook.java

---
+++ com/learn/command/orderFood/ICook.java
@@ -0,0 +1,14 @@
+package com.learn.command.orderFood;
+
+/**
+ * @ProjectName: [design-patterns]
+ * @Package: com.learn.command.orderFood
+ * @ClassName: ICook
+ * @Description:厨师接口
+ * @Author: [wangmeng]
+ * @CreateDate: 2021/4/4 15:22
+ * @Version: V1.0
+ */
+public interface ICook {
+    void action();
+}
