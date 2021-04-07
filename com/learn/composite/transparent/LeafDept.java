7cdaabbfbb21c9f0763d3f254b402663bea344d0
wm1990 <39044206+wm1990@users.noreply.github.com>
2021-04-07 13:10:10 +0800

    Update LeafDept.java

--- com/learn/composite/transparent/LeafDept.java
+++ com/learn/composite/transparent/LeafDept.java
@@ -10,7 +10,6 @@ import java.util.List;
  * @Author: [wangmeng]
  * @CreateDate: 2021/3/30 22:55
  * @Version: V1.0
- * Copyright: 北京宏图天安 Copyright (c) 2021
  */
 public class LeafDept implements DeptComponent {
     private String name;
