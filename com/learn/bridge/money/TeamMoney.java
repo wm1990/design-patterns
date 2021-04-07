7473c833cf55a7ffd08b9c07366aa51efbd93457
wm1990 <39044206+wm1990@users.noreply.github.com>
2021-04-07 12:49:44 +0800

    Update TeamMoney.java

--- com/learn/bridge/money/TeamMoney.java
+++ com/learn/bridge/money/TeamMoney.java
@@ -8,7 +8,6 @@ package com.learn.bridge.money;
  * @Author: [wangmeng]
  * @CreateDate: 2021/4/7 11:36
  * @Version: V1.0
- * Copyright: 北京宏图天安 Copyright (c) 2021
  */
 public class TeamMoney implements Money{
     @Override
