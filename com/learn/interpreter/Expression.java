25899da48ca8c664708f6bfcb1d7a2d815a9b5f2
wm1990 <39044206+wm1990@users.noreply.github.com>
2021-04-08 23:53:30 +0800

    Create Expression.java

---
+++ com/learn/interpreter/Expression.java
@@ -0,0 +1,14 @@
+package com.learn.interpreter;
+
+/**
+ * @ProjectName: [design-patterns]
+ * @Package: com.learn.interpreter
+ * @ClassName: Expression
+ * @Description:抽象表达式类
+ * @Author: [wangmeng]
+ * @CreateDate: 2021/4/8 22:31
+ * @Version: V1.0
+ */
+public interface Expression {
+    int interpret();
+}
